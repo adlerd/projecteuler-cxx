1f5798ff8ba548357710ecd72b8196760dff66dd
David Adler <d.adler.s@gmail.com>
2012-02-28 18:50:49 -0500

    Added problems 5, 6, and 7.

--- src/set0.cc
+++ src/set0.cc
@@ -1,7 +1,9 @@
 #include <stdexcept>
+#include <utility>
 
 #include "util.hh"
 #include "set0.hh"
+#include "atkin.hh"
 
 namespace euler {
     std::string problem1(){
@@ -44,7 +46,48 @@ namespace euler {
 	    }
 	return std::to_string(high);
     }
+    ulong gcd(ulong a, ulong b){
+	if(a * b == 0)
+	    return a | b;
+	unsigned int shift;
+	for(shift = 0; ((a|b) & 1) == 0; ++shift){
+	    a >>= 1;
+	    b >>= 1;
+	}
+	while((a & 1) == 0)
+	    a >>= 1;
+	do {
+	    while((b & 1) == 0)
+		b >>= 1;
+	    if(a > b)
+		std::swap(a, b);
+	    b -= a;
+	} while(b != 0);
+	return a << shift;
+    }
+    std::string problem5(){
+	ulong g = 1;
+	for(ulong i = 2; i <= 20; ++i)
+	    g = (g * i) / gcd(g, i);
+	return std::to_string(g);
+    }
+    std::string problem6(){
+	ulong sum = 0;
+	ulong sum_sq = 0;
+	for(ulong i = 1; i <= 100; ++i){
+	    sum += i;
+	    sum_sq += i * i;
+	}
+	sum *= sum;
+	return std::to_string(sum > sum_sq ? sum - sum_sq : sum_sq - sum);
+    }
+    std::string problem7(){
+	prime_iterator pi;
+	for(int i = 0; i < 10000; ++i)
+	    ++pi;
+	return std::to_string(*pi);
+    }
 #define P(x) {x, &problem ## x}
     std::list<problem> set0
-    {{P(1), P(2), P(3), P(4)}};
+    {{P(1), P(2), P(3), P(4), P(5), P(6), P(7)}};
 }
