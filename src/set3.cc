462e300e6868fc3ca42f72b8e5cc0e23dddf960f
David Adler <d.adler.s@gmail.com>
2012-03-16 09:35:05 -0400

    Added problem 32.

--- src/set3.cc
+++ src/set3.cc
@@ -1,4 +1,6 @@
 #include <array>
+#include <algorithm>
+#include <set>
 
 #include "set3.hh"
 #include "util.hh"
@@ -43,7 +45,33 @@ namespace euler {
 	cl(0);
 	return std::to_string(ct);
     }
+    template <class Iter>
+	ulong from_digits(Iter begin, Iter end){
+	    ulong s = 0;
+	    while(begin != end){
+		s *= 10;
+		s += *begin++;
+	    }
+	    return s;
+	}
+    std::string problem32(){
+	std::array<unsigned char, 9> digits = {{1, 2, 3, 4, 5, 6, 7, 8, 9}};
+	std::set<ulong> products;
+	do {
+	    ulong back = from_digits(digits.cbegin() + 5, digits.cend());
+	    for(unsigned int const split : {1,2}){
+		ulong p = from_digits(digits.cbegin(), digits.cbegin()+split)
+		    * from_digits(digits.cbegin()+split, digits.cbegin() + 5);
+		if(p == back)
+		    products.insert(p);
+	    }
+	} while(std::next_permutation(digits.begin(), digits.end()));
+	ulong s = 0;
+	for(ulong p : products)
+	    s += p;
+	return std::to_string(s);
+    }
 #define P(x) {x, &problem ## x}
     std::list<problem> set3
-    {{P(30),P(31)}};
+    {{P(30),P(31),P(32)}};
 }
