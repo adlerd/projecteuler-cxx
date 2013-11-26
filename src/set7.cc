b1e3b0c2bc528364fd86888164dae9b590a0eae4
David Adler <d.adler.s@gmail.com>
2013-11-25 22:07:29 -0500

    Added problems 75, 76, and 77.

--- src/set7.cc
+++ src/set7.cc
@@ -103,7 +103,113 @@ namespace euler {
 	}
 	return ct;
     }
+    uint problem75(){
+	class tset {
+	    uint a;
+	    uint b;
+	public:
+	    tset() : a(0), b(0) {}
+	    tset(tset const&) = default;
+	    tset(tset&&) = default;
+	    void add(uint x, uint y){
+		if(a == 0){
+		    if(b == 0){
+			a = x;
+			b = y;
+		    }
+		} else if(!(a == x && b == y)){
+		    a = 0;
+		    b = 1;
+		}
+	    }
+	    bool one(){
+		return a != 0;
+	    }
+	};
+	uint constexpr limit = 1500000;
+	//std::array<tset,limit> zom;
+	std::vector<tset> zom(limit);
+	for(uint m = 1; m < 867; ++m){
+	    for(uint n = 1; n < m; ++n){
+		uint const p = 2*m*(n+m);
+		if(p > limit)
+		    break;
+		if(gcd(m,n) == 1){
+		    uint x = m*m-n*n;
+		    uint y = 2*m*n;
+		    if(x > y)
+			std::swap(x,y);
+		    uint pp = p;
+		    uint xx = x;
+		    uint yy = y;
+		    do {
+			zom[pp-1].add(xx,yy);
+			pp+=p;
+			xx+=x;
+			yy+=y;
+		    } while(pp <= limit);
+		}
+	    }
+	}
+	uint ct = 0;
+	for(auto& z : zom){
+	    if(z.one())
+		++ct;
+	}
+	return ct;
+    }
+
+    class partitions {
+	std::vector<uint> memo;
+	uint size;
+	static uint trinum(uint n){
+	    return (n*(n+1))/2;
+	}
+	uint parts(uint n, uint k);
+	typedef bool (* const validt)(ulong);
+    public:
+	explicit partitions(validt v) : memo(0), size(0), valid(v) {}
+	partitions(partitions const&) = default;
+	partitions(partitions&&) = default;
+	validt valid;
+	uint operator[](uint i);
+    };
+    uint partitions::operator[](uint i){
+	if(i >= size){
+	    size = i+1;
+	    memo.resize(trinum(size));
+	}
+	return parts(i, 1);
+    }
+    uint partitions::parts(uint n, uint k){
+	if(k > n)
+	    return 0;
+	uint& a = memo[trinum(n)+k];
+	if(!a) {
+	    if(k == n)
+		a = (*valid)(n) ? 1 : 0;
+	    else if((*valid)(k))
+		a = parts(n, k+1) + parts(n-k, k);
+	    else
+		a = parts(n, k+1);
+	}
+	return a;
+    }
+
+    bool always_true(ulong){
+	return true;
+    }
+    uint problem76(){
+	return partitions(&always_true)[100]-1;
+    }
+    uint problem77(){
+	uint x = 11;
+	partitions p(&is_prime);
+	while(p[x] <= 5000)
+	    ++x;
+	return x;
+    }
 #define P(x) new_problem(x, &problem ## x)
     std::list<problem const*> set7
-    {{P(70),P(71),P(72),P(73),P(74)}};
+    {{P(70),P(71),P(72),P(73),P(74),P(75),P(76),P(77)}};
 }
