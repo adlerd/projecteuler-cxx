ef86aa74b081ced0d7aa84d4e9e9e8206509e981
David Adler <d.adler.s@gmail.com>
2014-01-05 21:18:07 -0500

    Added problem 84.

--- src/set8.cc
+++ src/set8.cc
@@ -3,6 +3,9 @@
 #include <cassert>
 #include <array>
 #include <queue>
+#include <memory>
+#include <sstream>
+#include <iomanip>
 
 namespace {
     using namespace euler;
@@ -143,9 +146,159 @@ namespace {
 	using namespace eighty_dijkstra;
 	return dijkstra(&problem83_neighbors);
     }
+    namespace monopoly {
+	uint constexpr width = 40*3;
+	uint constexpr die_side_ct = 4;
+	uint constexpr roll_ways = die_side_ct * die_side_ct;
+	typedef double val_t;
+	typedef std::array<val_t,width> col_t; // each elt is a dest
+	typedef std::array<col_t, width> mat_t; // each elt (column) is a src
+	void mat_square_into(mat_t& into, mat_t const& from){
+	    for(uint i = 0; i < width; ++i){
+		for(uint j = 0; j < width; ++j){
+		    val_t accum = 0;
+		    for(uint k = 0; k < width; ++k)
+			accum += from[i][k] * from[k][j];
+		    into[i][j] = accum;
+		}
+	    }
+	}
+	inline uint modforty(uint x){
+	    /* divisionless simple modulo forty. of course, this introduces a
+	     * branch... */
+	    return (x < 40 ? x : x - 40);
+	}
+	struct state {
+	    uchar square;
+	    uchar doubles;
+	    uint to_index(){
+		assert(square < 40 && doubles < 3);
+		return square + 40 * doubles;
+	    }
+	    state with_roll(uint val, bool dbl){
+		state ret{(uchar)modforty(val + square),
+		          (uchar)(dbl ? 1+doubles : 0)};
+		if(ret.doubles > 2)
+		    return {10,0};
+		else
+		    return ret;
+	    }
+	};
+	void resolve_cc(col_t& vec, state dest, val_t chance){
+	    val_t small = chance / 16;
+	    vec[state{0,dest.doubles}.to_index()] += small;
+	    vec[state{10,0}.to_index()] += small;
+	    vec[dest.to_index()] += 14 * small;
+	}
+	struct ch_vars {
+	    uchar next_rr;
+	    uchar next_u;
+	    uchar back_3;
+	    bool trigger_cc;
+	};
+	void resolve_ch(col_t& vec, state dest, val_t chance, ch_vars vars){
+	    val_t small = chance / 16;
+	    uchar dbls = dest.doubles;
+	    vec[state{0,dbls}.to_index()] += small;
+	    vec[state{10,0}.to_index()] += small;
+	    vec[state{11,dbls}.to_index()] += small;
+	    vec[state{24,dbls}.to_index()] += small;
+	    vec[state{39,dbls}.to_index()] += small;
+	    vec[state{5,dbls}.to_index()] += small;
+	    vec[state{vars.next_rr,dbls}.to_index()] += 2 * small;
+	    vec[state{vars.next_u,dbls}.to_index()] += small;
+	    vec[dest.to_index()] += 6 * small;
+	    state target = state{vars.back_3,dbls};
+	    if(vars.trigger_cc)
+		resolve_cc(vec,target,small);
+	    else
+		vec[target.to_index()] += small;
+	}
+	void resolve(col_t& vec, state dest, val_t chance){
+	    uchar sq = dest.square;
+	    if(sq == 2 || sq == 18 || sq == 33){
+		resolve_cc(vec,dest,chance);
+	    } else if(sq == 7){
+		resolve_ch(vec,dest,chance,{15,12,4,false});
+	    } else if(sq == 22){
+		resolve_ch(vec,dest,chance,{25,28,19,false});
+	    } else if (sq == 36){
+		resolve_ch(vec,dest,chance,{5,12,33,true});
+	    } else if(sq == 30){
+		vec[state{10,0}.to_index()] += chance;
+	    } else {
+		vec[dest.to_index()] += chance;
+	    }
+	}
+	std::vector<std::pair<uint, val_t>> s_rolls(){
+	    std::vector<val_t> vec(2*die_side_ct);
+	    for(uint x = 1; x <= die_side_ct; ++x)
+		for(uint y = x+1; y <= die_side_ct; ++y)
+		    vec[x+y] += 2;
+	    decltype(s_rolls()) ret;
+	    for(uint i = 0; i < vec.size(); ++i){
+		if(vec[i] != 0)
+		    ret.push_back({i, vec[i] / roll_ways});
+	    }
+	    return ret;
+	}
+	std::vector<std::pair<uint, val_t>> run_alg(){
+	    std::unique_ptr<mat_t> a(new mat_t);
+	    for(uint i = 0; i < width; ++i)
+		for(uint j = 0; j < width; ++j)
+		    (*a)[i][j] = 0;
+	    auto srolls = s_rolls();
+	    // fill matrix for single state transition
+	    for(uint sq = 0; sq < 40; ++sq){
+		for(uint dbls = 0; dbls < 3; ++dbls){
+		    state src{(uchar)sq,(uchar)dbls};
+		    auto& col = (*a)[src.to_index()];
+		    for(auto p : srolls)
+			resolve(col,src.with_roll(p.first,false),p.second);
+		    for(uint d = 1; d <= die_side_ct; ++d)
+			resolve(col,src.with_roll(d*2,true),1.0/roll_ways);
+		}
+	    }
+	    // run simulation 2^6 times
+	    {
+		std::unique_ptr<mat_t> b(new mat_t);
+		mat_square_into(*b,*a);//2
+		mat_square_into(*a,*b);//4
+		mat_square_into(*b,*a);//8
+		mat_square_into(*a,*b);//16
+		mat_square_into(*b,*a);//32
+		mat_square_into(*a,*b);//64
+	    }
+	    decltype(run_alg()) ret;
+	    for(uint dest = 0; dest < 40; ++dest){
+		val_t ch = 0;
+		for(uint src = 0; src < width; ++src){
+		    ch += (*a)[src][dest];
+		    ch += (*a)[src][dest+40];
+		    ch += (*a)[src][dest+80];
+		}
+		ret.emplace_back(dest,ch);
+	    }
+	    return ret;
+	}
+	typedef decltype(run_alg()[0]) result_item;
+	bool comp_result(result_item& f, result_item& s){
+	    return f.second > s.second;
+	}
+    }
+    std::string problem84(){
+	using namespace monopoly;
+	auto result = run_alg();
+	std::partial_sort(result.begin(), result.begin()+3,
+		result.end(),&comp_result);
+	std::ostringstream out;
+	for(int i = 0; i < 3; ++i)
+	    out << std::setfill('0') << std::setw(2) << result[i].first;
+	return out.str();
+    }
 }
 namespace euler {
 #define P(x) new_problem(x, &problem ## x)
     std::list<problem const*> set8
-    {{P(80),P(81),P(82),P(83)}};
+    {{P(80),P(81),P(82),P(83),P(84)}};
 }
