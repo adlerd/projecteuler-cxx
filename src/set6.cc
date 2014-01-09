4563a300eaf8898952e7b4bb0b5a148690329608
David Adler <d.adler.s@gmail.com>
2014-01-08 19:19:20 -0500

    Rewrote problem 60.

--- src/set6.cc
+++ src/set6.cc
@@ -8,107 +8,81 @@
 #include <array>
 #include <set>
 #include <bitset>
+#include <iterator>
 
 namespace {
     using namespace euler;
     namespace euler60 {
-	uint constexpr TARGET_DEPTH = 5;
-	struct node;
-	typedef std::map<ulong, bool> bmap;
-	typedef std::list<node> nlist;
-	struct node {
-	    ulong head;
-	    nlist children;
-	    explicit node(ulong h) : head(h), children() {}
-	};
-	ulong next_10(ulong b){
-	    ulong a = 1;
-	    while(b > 0){
-		a *= 10;
-		b /= 10;
-	    }
-	    return a;
-	}
-	void make_matchmap(ulong prime, bmap& into){
-	    into.clear();
-	    ulong const factor_a = next_10(prime);
-	    ulong factor_b = 10;
-	    prime_iterator it(6);
-	    ulong check_prime = 3;
-	    while(check_prime < prime){
-		while(factor_b <= check_prime)
-		    factor_b *= 10;
-		into[check_prime] = is_prime(factor_a * check_prime + prime)
-		    && is_prime(factor_b * prime + check_prime);
-		check_prime = *it++;
-	    }
-	}
-	struct state {
+	struct pdef {
+	    // it's just std::pair, but needs a name to avoid infinite type
 	    ulong prime;
-	    bmap match;
-	    node top;
-	    ulong minsum;
-	    state() : prime(0), top(0), minsum(0) {}
+	    std::vector<pdef const*> m;
 	};
-	bool refilter(node& from, ulong next_prime, uint depth, ulong sum){
-	    nlist& ch = from.children;
-	    if(sum < from.head)
-		return true;
-	    sum -= from.head;
-	    uint const next_depth = depth + 1;
-	    ch.remove_if([next_prime, next_depth, sum] (node& n) {
-		    return refilter(n, next_prime, next_depth, sum); });
-	    return ch.empty() && sum < next_prime * (TARGET_DEPTH - depth);
+	typedef std::vector<pdef> store;
+	typedef std::list<pdef const*> matchlist;
+	store make_primes(){
+	    store ret;
+	    ret.push_back(pdef{3});
+	    for(prime_iterator pi(6); *pi < 10000; ++pi)
+		ret.push_back(pdef{*pi});
+	    return ret;
 	}
-	/* pre: prime should actually go in node */
-	void insert_into_node(node& node, ulong cursum, uint depth,
-		state& state){
-	    cursum += node.head;
-	    struct node next(state.prime);
-	    if(depth == TARGET_DEPTH - 1){
-		ulong prime = state.prime;
-		assert (state.minsum == 0 || cursum < state.minsum);
-		state.minsum = cursum + prime;
-		node.children.push_back(next);
-		refilter(state.top, state.prime, 0, state.minsum);
-	    } else {
-		for(struct node& n : node.children)
-		    if(state.match[n.head])
-			insert_into_node(n, cursum, depth + 1, state);
-		node.children.push_back(next);
+	void make_matchmap(store& info){
+	    decltype(pdef::m) tmp;
+	    uint const size = info.size();
+	    ulong order_i = 10;
+	    for(uint i = 0; i < size; ++i){
+		ulong const pi = info[i].prime;
+		if(order_i <= pi)
+		    order_i *= 10;
+		ulong order_j = order_i;
+		for(uint j =  i + 1; j < size; ++j){
+		    ulong const pj = info[j].prime;
+		    if(order_j <= pj)
+			order_j *= 10;
+		    if(is_prime(pi + order_i * pj) && is_prime(pj + order_j * pi))
+			tmp.push_back(&info[j]);
+		}
+		tmp.shrink_to_fit();
+		std::swap(tmp, info[i].m);
 	    }
 	}
-	/* one might sacrifice algorithmic correctness for this faster solution:
-	bool is_complete(state& state){
-	    return state.minsum != 0;
+	bool compare_pdef(pdef const* a, pdef const* b){
+	    return a->prime < b->prime;
 	}
-	*/
-	bool is_complete(state& state){
-	    if(!state.minsum)
-		return false;
-	    node *n = &state.top;
-	    for(uint i = 0; i < TARGET_DEPTH; ++i){
-		if(n->children.size() != 1)
-		    return false;
-		n = &n->children.front();
+	ulong rec(matchlist const& matches, uint const n){
+	    uint const req = 5 - n;
+	    if(matches.size() < req)
+		return 0;
+	    if(n == 4)
+		return matches.front()->prime;
+	    auto end = matches.cend();
+	    std::advance(end, -static_cast<int>(req - 1));
+	    auto iter = matches.cbegin();
+	    while(iter != end){
+		matchlist out;
+		auto val = *iter++;
+		std::set_intersection(iter, matches.cend(),
+			val->m.cbegin(), val->m.cend(),
+			std::back_insert_iterator<matchlist>(out),&compare_pdef);
+		ulong res = rec(out,n+1);
+		if(res)
+		    return res + val->prime;
 	    }
-	    assert(n->children.size() == 0);
-	    return true;
+	    return 0;
 	}
     }
     ulong problem60(){
 	using namespace euler60;
-	state state;
-	state.prime = 3;
-	prime_iterator iter(6);
-	while(!is_complete(state)){
-	    make_matchmap(state.prime, state.match);
-	    insert_into_node(state.top, state.minsum, 0, state);
-	    state.prime = *iter++;
-	    if(state.minsum)
-		refilter(state.top, state.prime, 0, state.minsum);
-	}
-	return state.minsum;
+	store store = make_primes();
+	make_matchmap(store);
+	matchlist master;
+	for(pdef const& p : store)
+	    master.push_back(&p);
+	ulong result = rec(master,0);
+	if(result == 0)
+	    throw std::logic_error("problem 60 unsolved");
+	return result;
     }
     namespace euler61 {
 	struct fourd {
