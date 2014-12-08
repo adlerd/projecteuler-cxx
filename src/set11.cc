3603e9c6a4b8df9b9e30bfd25de75730bf940f86
David Adler <d.adler.s@gmail.com>
2014-12-08 17:38:50 -0500

    Add problems 113 and 114.

--- src/set11.cc
+++ src/set11.cc
@@ -120,9 +120,66 @@ namespace {
 	}
 	return d;
     }
+    namespace euler113 {
+	typedef std::vector<std::vector<u64>> mem_t;
+	u64 increasing(mem_t& incr, u8 digct, u8 min){
+	    if(min == 9 || digct == 0)
+		return 1;
+	    if(incr[digct][min] != 0)
+		return incr[digct][min];
+	    u64 ret = 0;
+	    for(u8 i = min; i < 10; ++i)
+		ret += increasing(incr, digct - 1, i);
+	    incr[digct][min] = ret;
+	    return ret;
+	}
+	u64 decreasing(mem_t& incr, u8 digct){
+	    // numbers which, when reversed (and losing their trailing zeros),
+	    // are increasing, but here we'll skip the all-same numbers
+	    if(digct < 2)
+		return 0;
+	    u64 ret = decreasing(incr, digct-1);
+	    for(u8 trailing = 0; trailing < digct; ++trailing)
+		ret += increasing(incr, digct - trailing, 1);
+	    return ret-9;
+	}
+    }
+    u64 problem113(){
+	using namespace euler113;
+	u8 constexpr digs = 100;
+	mem_t incr(digs+1,std::vector<u64>(10,0));
+	return increasing(incr, digs, 0) + decreasing(incr, digs) - 1;
+    }
+    namespace euler114 {
+	typedef std::vector<u64> mem_t;
+	u64 start_red(mem_t& mem, u8 len){
+	    if(len < 3)
+		return 0;
+	    if(mem[len] != 0)
+		return mem[len];
+	    u64 ret = 1; // the all-red solution
+	    for(u8 rlen = 3; rlen < len; ++rlen){
+		ret += 1; // a red-then-all-black solution
+		for(u8 blen = 1; blen < (len - rlen); ++blen){
+		    ret += start_red(mem, len - rlen - blen);
+		}
+	    }
+	    mem[len] = ret;
+	    return ret;
+	}
+    }
+    u64 problem114(){
+	using namespace euler114;
+	u8 constexpr len = 50;
+	mem_t mem(len+1);
+	u64 ret = 1; // the all-black solution
+	for(u8 blen = 0; blen < len; ++blen)
+	    ret += start_red(mem, len - blen);
+	return ret;
+    }
 }
 namespace euler {
 #define P(x) new_problem(x, &problem ## x)
     std::list<problem const*> set11
-    {P(110),P(111),P(112)};
+    {P(110),P(111),P(112),P(113),P(114)};
 }
