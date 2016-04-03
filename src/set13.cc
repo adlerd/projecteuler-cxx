e6e727cbf6663d6af18647e3b0d210920ee297a8
David Adler <adlerd@secundaverba.net>
2016-04-03 15:25:01 -0700

    Add problem 137.

--- src/set13.cc
+++ src/set13.cc
@@ -157,10 +157,29 @@ namespace {
 	}
 	return ret;
     }
-
+    bigint problem137(){
+	// the limit of the generating series for Fibonacci numbers
+	// is x/(1-x-x^2); if the value of this is an integer n then
+	// x is rational iff 1+2n+5n^2 is a square
+	/*
+	for(u64 a = 1; a < (1<<30) ; ++a){
+	    if(isqrt(1+2*a+5*a*a) != 0)
+		std::cerr << a << std::endl;
+	}*/
+	// oeis gives F(2n)F(2n+1) for the above sequence, so...
+	bigint a = 0;
+	bigint b = 1;
+	u8 i = 1;
+	while(i < 31){
+	    a += b;
+	    std::swap(a,b);
+	    ++i;
+	}
+	return a*b;
+    }
 }
 namespace euler {
 #define P(x) new_problem(x, &problem ## x)
     std::list<problem const*> set13
-    {P(130),P(131),P(132),P(133),P(134),P(135),P(136)};
+    {P(130),P(131),P(132),P(133),P(134),P(135),P(136),P(137)};
 }
